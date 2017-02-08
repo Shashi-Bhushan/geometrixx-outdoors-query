4a8bd0c363c8a6cb7a7e9b672dbb7d42e4aab4c3
Shashi Bhushan <sbhushan1@outlook.com>
2017-02-08 16:24:30 +0530

    Working Template

--- content/src/main/content/jcr_root/etc/clientlibs/geometrixx-outdoors-query/main/js/main.js
+++ content/src/main/content/jcr_root/etc/clientlibs/geometrixx-outdoors-query/main/js/main.js
@@ -1,5 +1,41 @@
 ;
 "use strict";
 (function($){
-  console.log("Working Lib");
+  $(document).ready(function(){
+    var source = $('#product-search-template').html();
+
+      var data = {
+                   "stats" : {
+                     "startIndex" : 1,
+                     "endIndex" : 2,
+                     "totalItems" : 2
+                   },
+                   "results" : [
+                     {
+                       "url" : "/content/geometrixx-outdoors/en/men/shirts/ashanti-nomad.html",
+                       "title" : "Ashanti Nomad",
+                       "date" : "Jun 11, 2013",
+                       "similar-url" : "/content/geometrixx-outdoors/en/toolbar/search.html?q=related%3A%2Fcontent%2Fgeometrixx-outdoors%2Fen%2Fmen%2Fshirts%2Fashanti-nomad"
+                     },
+                     {
+                       "url" : "/content/geometrixx-outdoors/en/activities/nairobi-runners-running.html",
+                       "title" : "Running",
+                       "date" : "Jun 11, 2013",
+                       "similar-url" : "/content/geometrixx-outdoors/en/toolbar/search.html?q=related%3A%2Fcontent%2Fgeometrixx-outdoors%2Fen%2Fmen%2Fshirts%2Fashanti-nomad"
+                     }
+                   ]
+                 };
+
+      var compiled = dust.compile(source, 'product-search-template');
+      dust.loadSource(compiled);
+
+    //  $('input[name=productSearchSubmit]').click(function(){
+    //    var textFieldValue = $('input[name=productSearch]').val();
+
+        dust.render('product-search-template', data, function(err, out){
+          $('#output').html(out);
+        });
+    //  });
+  });
+
 })(jQuery);
