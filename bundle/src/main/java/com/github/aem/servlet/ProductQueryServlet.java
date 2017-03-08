6e0516218b08692890e554e6e338521166924529
Shashi Bhushan <sbhushan1@outlook.com>
2017-03-08 15:13:06 +0530

    Search Term Updated

--- bundle/src/main/java/com/github/aem/servlet/ProductQueryServlet.java
+++ bundle/src/main/java/com/github/aem/servlet/ProductQueryServlet.java
@@ -5,17 +5,22 @@ import com.day.cq.search.Query;
 import com.day.cq.search.QueryBuilder;
 import com.day.cq.search.result.Hit;
 import com.day.cq.search.result.SearchResult;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.*;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,9 +28,10 @@ import java.util.Map;
  * Created by shabhushan on 2/23/2017.
  */
 @Component(label="Product Query Servlet", description = "Returns Query Results in json format")
+@Service(Servlet.class)
 @Properties({
   @Property(name="sling.servlet.paths", value="/bin/product-search"),
-  @Property(name="sling.servlet.methods", value="get"),
+  @Property(name="sling.servlet.methods", value="GET"),
   @Property(name="sling.servlet.extensions", value="json")
 })
 public class ProductQueryServlet extends SlingSafeMethodsServlet {
@@ -33,17 +39,57 @@ public class ProductQueryServlet extends SlingSafeMethodsServlet {
   @Reference
   private QueryBuilder queryBuilder;
 
+  private static final String[] MONTHS = {
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  };
+
   @Override
   protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-    super.doGet(request, response);
+//    super.doGet(request, response);
     response.setHeader("Content-Type", "application/json");
+
+    JSONArray array = null;
+
+    try {
+      array = getResults(request);
+    } catch (RepositoryException e) {
+      e.printStackTrace();
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+
+    response.getWriter().write(array.toString());
 
   }
 
-  private void getResults(SlingHttpServletRequest request) {
+  private JSONArray getResults(SlingHttpServletRequest request) throws RepositoryException, JSONException {
+    JSONArray searchResults = new JSONArray();
+
     Map<String, String> searchCriteria = new HashMap<String, String>();
 
-    searchCriteria.put("path", "/etc/commerce/products");
+    searchCriteria.put("path", "/etc/commerce/products/geometrixx-outdoors");
+    searchCriteria.put("type", "nt:unstructured");
+
+    searchCriteria.put("1_property", "sling:resourceType");
+    searchCriteria.put("1_property.value", "commerce/components/product");
+
+    searchCriteria.put("2_property", "jcr:title");
+    searchCriteria.put("2_property.value", request.getParameter("searchTerm"));
+
+    searchCriteria.put("3_property", "cq:commerceType");
+    searchCriteria.put("3_property.1_value", "product");
+    searchCriteria.put("3_property.2_value", "variant");
 
 
     PredicateGroup predicateGroup = PredicateGroup.create(searchCriteria);
@@ -52,7 +98,17 @@ public class ProductQueryServlet extends SlingSafeMethodsServlet {
     SearchResult searchResult = query.getResult();
 
     for(Hit hit: searchResult.getHits()) {
+      JSONObject result = new JSONObject();
+      result.put("url", hit.getPath());
 
+      ValueMap properties = hit.getProperties();
+      result.put("title", String.valueOf(properties.get("jcr:title")));
+      Calendar calendar = (GregorianCalendar)properties.get("jcr:lastModified");
+      result.put("date", String.valueOf(MONTHS[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.YEAR) + "," + calendar.get(Calendar.DATE)));
+
+      searchResults.put(result);
     }
+
+    return searchResults;
   }
 }
--- content/src/main/content/jcr_root/etc/clientlibs/geometrixx-outdoors-query/main/js/main.js
+++ content/src/main/content/jcr_root/etc/clientlibs/geometrixx-outdoors-query/main/js/main.js
@@ -4,38 +4,52 @@
   $(document).ready(function(){
     var source = $('#product-search-template').html();
 
-      var data = {
-                   "stats" : {
-                     "startIndex" : 1,
-                     "endIndex" : 2,
-                     "totalItems" : 2
-                   },
-                   "results" : [
-                     {
-                       "url" : "/content/geometrixx-outdoors/en/men/shirts/ashanti-nomad.html",
-                       "title" : "Ashanti Nomad",
-                       "date" : "Jun 11, 2013",
-                       "similar-url" : "/content/geometrixx-outdoors/en/toolbar/search.html?q=related%3A%2Fcontent%2Fgeometrixx-outdoors%2Fen%2Fmen%2Fshirts%2Fashanti-nomad"
+
+    var compiled = dust.compile(source, 'product-search-template');
+    dust.loadSource(compiled);
+
+      $('input[name=productSearchSubmit]').click(function(){
+        var textFieldValue = $('input[name=productSearch]').val();
+
+        var data = {
+                     "stats" : {
+                       "startIndex" : 1,
+                       "endIndex" : 2,
+                       "totalItems" : 2
                      },
-                     {
-                       "url" : "/content/geometrixx-outdoors/en/activities/nairobi-runners-running.html",
-                       "title" : "Running",
-                       "date" : "Jun 11, 2013",
-                       "similar-url" : "/content/geometrixx-outdoors/en/toolbar/search.html?q=related%3A%2Fcontent%2Fgeometrixx-outdoors%2Fen%2Fmen%2Fshirts%2Fashanti-nomad"
-                     }
-                   ]
-                 };
+                     "results" : [
+                       {
+                         "url" : "/content/geometrixx-outdoors/en/men/shirts/ashanti-nomad.html",
+                         "title" : "Ashanti Nomad",
+                         "date" : "Jun 11, 2013",
+                         "similar-url" : "/content/geometrixx-outdoors/en/toolbar/search.html?q=related%3A%2Fcontent%2Fgeometrixx-outdoors%2Fen%2Fmen%2Fshirts%2Fashanti-nomad"
+                       },
+                       {
+                         "url" : "/content/geometrixx-outdoors/en/activities/nairobi-runners-running.html",
+                         "title" : "Running",
+                         "date" : "Jun 11, 2013",
+                         "similar-url" : "/content/geometrixx-outdoors/en/toolbar/search.html?q=related%3A%2Fcontent%2Fgeometrixx-outdoors%2Fen%2Fmen%2Fshirts%2Fashanti-nomad"
+                       }
+                     ]
+                   };
+        $.ajax({
+          url : '/bin/product-search.json',
+          method : 'GET',
+          dataType : 'json',
+          data : {
+            "searchTerm" : textFieldValue
+          },
+          success : function(response) {
+            console.log(response);
+            data.results = response;
 
-      var compiled = dust.compile(source, 'product-search-template');
-      dust.loadSource(compiled);
-
-    //  $('input[name=productSearchSubmit]').click(function(){
-    //    var textFieldValue = $('input[name=productSearch]').val();
-
-        dust.render('product-search-template', data, function(err, out){
-          $('#output').html(out);
+            // Render the Data
+            dust.render('product-search-template', data, function(err, out){
+              $('#output').html(out);
+            });
+          }
         });
-    //  });
+      });
   });
 
 })(jQuery);
